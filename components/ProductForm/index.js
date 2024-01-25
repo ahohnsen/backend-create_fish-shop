39fa4120bb1ce3b4b0be00aeea904913ccb1819d
Andrea Hohnsen <a.hohnsen@mail.de>
2024-01-25 11:02:46 +0100

    Add solution

--- components/ProductForm/index.js
+++ components/ProductForm/index.js
@@ -1,12 +1,25 @@
+import useSWR from "swr";
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
 
 export default function ProductForm() {
+  const { mutate } = useSWR("/api/products");
+
   async function handleSubmit(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const productData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
   }
 
   return (
